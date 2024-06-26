0a858daf16bd6f5177901e4b0827286066475304
MiaoRiDzo <ya.eltyshev@gmail.com>
2024-06-26 16:34:17 +0500

    switch to universal method update

--- DB/updateObj.py
+++ DB/updateObj.py
@@ -2,67 +2,14 @@ from config import *
 from model import *
 from sqlalchemy.orm import Session
 
-#Cabinet
-def updateCabinetID(id, CabinetNum, CabinetName, zone):
+def update_object(model_class, id, **kwargs):
     session = Session(engine)
     
-    obj = session.query(Cabinet).get(id)
+    obj = session.query(model_class).get(id)
     
-    obj.CabinetNum = CabinetNum
-    obj.CabinetName = CabinetName
-    obj.ZoneID = zone.ZoneID
+    for key, value in kwargs.items():
+        setattr(obj, key, value)
     
     session.add(obj)
     session.commit()
     session.close()
-
-#Equipment
-def updateEqStatusID(id, eqType, eqStat, eqCab, EqName, SN):
-    session = Session(engine)
-
-    obj = session.query(Equipment).get(id)
-
-    obj.EquipmentTypeID = eqType.EquipmentTypeID
-    obj.EquipmentStatusID = eqStat.EquipmentStatusID
-    obj.CabinetID = eqCab.CabinetID
-
-    obj.EquipmentName = EqName
-    obj.SN = SN
-
-#EqStatus
-def updateEqStatusID(id, StatusName):
-    session = Session(engine)
-    
-    obj = session.query(EquipmentStatus).get(id)
-    
-    obj.EquipmentStatusName = StatusName
-
-    session.add(obj)
-    session.commit()
-    session.close()
-
-#EqType
-def updateEqTypeID(id, TypeName):
-    session = Session(engine)
-    
-    obj = session.query(EquipmentType).get(id)
-    
-    obj.EquipmentTypeName = TypeName
-
-    session.add(obj)
-    session.commit()
-    session.close()
-
-#Zone
-def updateZoneID(id, zoneName, Floor):
-    session = Session(engine)
-    
-    obj = session.query(Zone).get(id)
-    
-    obj.ZoneName = zoneName
-    obj.Floor = Floor 
-    
-    session.add(obj)
-    session.commit()
-    session.close()
-
